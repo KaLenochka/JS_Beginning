d71cf0a0fb94349a06691f33ee34e08f0fd12bb3
KaLenochka <elena.dolinchuk2018@gmail.com>
2022-05-31 16:24:09 +0200

    31_05_2022

---
+++ concantinationOfStrings.js
@@ -0,0 +1,13 @@
+const hello = 'Hello'
+const world = 'World'
+const greeting = hello + ' ' + world
+//шаблонные строки
+const greeting2 = `${hello} ${world}`
+console.log(greeting)
+console.log(greeting2)
+
+const name = 'Lena'
+const city = 'Wroclaw'
+const hi = `My name is ${name} and I live in ${city}`
+
+console.log(hi)
